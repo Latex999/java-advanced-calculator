83c026b60c09703d32768177116b6358589a8a55
Latex999 <M.Hassan17877@student.aast.edu>
2025-03-09 19:49:52 +0200

    Add Main application class

---
+++ src/main/java/com/calculator/Main.java
@@ -0,0 +1,13 @@
+package com.calculator;
+
+import com.calculator.ui.CalculatorApp;
+import javafx.application.Application;
+
+/**
+ * Main entry point for the Advanced Calculator application.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(CalculatorApp.class, args);
+    }
+}
